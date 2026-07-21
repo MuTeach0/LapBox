735ae6fd53c27eecd09b7bd8b14e7c18a9d98a54
MuTeach0 <m.ahmed562011@email.com>
2026-07-21 10:00:38 +0300

    AddChanges

---
+++ src/LapBox.Infrastructure/Data/Migrations/20260721065355_CartItemAsRegularEntity.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LapBox.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CartItemAsRegularEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems",
+                column: "Id")
+                .Annotation("SqlServer:Clustered", false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems",
+                column: "Id");
+        }
+    }
+}
